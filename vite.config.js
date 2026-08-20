756d3fb74bf8b23cb9a01b7a5a356b8f10d7d881
mohdwaqas20 <waqasmohd123786@gmail.com>
2026-08-20 08:01:28 +0400

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Venture_Tourism/',
+  base: '/Venture-Tourism/', 
   server: {
     port: 5173
   }
